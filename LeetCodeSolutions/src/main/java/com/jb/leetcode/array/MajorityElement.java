b883f52e7440af02fece09ea538c2ab95e3f4070
jbhushan791 <bhushan.jyoti791@gmail.com>
2020-12-31 03:52:44 -0500

    Majority elements

---
+++ LeetCodeSolutions/src/main/java/com/jb/leetcode/array/MajorityElement.java
@@ -0,0 +1,75 @@
+package com.jb.leetcode.array;
+
+/**
+ * Determine whether the give number is majority element in the given array
+ *
+ * @author jbhushan
+ */
+public class MajorityElement {
+
+    public static void main(String[] args){
+        int[] arr = {1,5,5,5,5,5,5,5,5,5,5,5,6,6};
+
+        long start = System.currentTimeMillis();
+        boolean isMajority = isMajority(arr, 6);
+        long stop = System.currentTimeMillis();
+        System.out.println(isMajority + " ");
+        System.out.println(stop-start);
+
+        long s = System.currentTimeMillis();
+        isMajority = isMajorityUsingBinarySearch(arr, 6);
+        long st = System.currentTimeMillis();
+        System.out.println(isMajority);
+        System.out.println(st-s);
+
+    }
+
+    public static boolean isMajority(int[] arr, int target) {
+        int l = 0;
+        if(arr.length % 2 == 0){
+            l = arr.length/2;
+        } else {
+           l = (arr.length/2) +1;
+        }
+        for(int i=0; i<l; i++){
+            if(target ==arr[i]  && arr[i] == arr[i+((arr.length/2))]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isMajorityUsingBinarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length -1;
+        int mid = 0;
+
+        // Using Binary Search, find the first index of the target present in array
+        while(left <= right){
+             mid = left + (right-left)/2;
+            if(arr[mid] == target){
+                // if array is reached till first index or mid-1 is
+                // not equal to target then it is not majority element
+                if(mid == 0 || arr[mid-1] != target){
+                    break;
+                }
+                right = mid-1;
+            } else if(arr[mid] < target){
+                left = mid+1;
+            } else if(arr[mid+1] > target){
+                right = mid-1;
+            }
+        }
+        return mid + arr.length/2 < arr.length && arr[mid + arr.length/2 ] == target;
+    }
+
+    public static int count(int start, int end, int[] nums, int count, int target){
+        for(int i = start; i<end; i++){
+            if(target == nums[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ LeetCodeSolutions/src/main/java/com/jb/leetcode/array/MajorityElement2.java
@@ -0,0 +1,70 @@
+package com.jb.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * return majority elements from array where count > n/3
+ *
+ * @author jbhushan
+ */
+public class MajorityElement2 {
+
+    public static void main(String[] args){
+        int[] arr = {1,5,5,5,5,4,4,4,4,4,5,5,6,6};
+
+        List<Integer> result = findMajorityUsingMoore(arr);
+
+        for(Integer n : result){
+            System.out.println(n);
+        }
+    }
+
+    public static List<Integer> findMajorityUsingMoore(int[] arr) {
+        Integer voter1=null;
+        int counter1 = 0;
+
+        Integer voter2=null;
+        int counter2 = 0;
+
+        for(int num: arr){
+            if(voter1 != null && voter1 == num){
+                counter1++;
+            } else if(voter2 != null && voter2 == num){
+                counter2++;
+            } else if(counter1 == 0){
+                voter1 = num;
+                counter1++;
+            }else if(counter2 == 0){
+                voter2 = num;
+                counter2++;
+            } else {
+                counter1--;
+                counter2--;
+            }
+        }
+
+        List<Integer> list = new ArrayList<Integer>(2);
+        counter1 = 0;
+        counter2 = 0;
+
+        for(int n : arr){
+            if (n == voter1) {
+                counter1++;
+            } else if (n == voter2) {
+                counter2++;
+            }
+        }
+
+        if(counter1 > arr.length/3){
+            list.add(voter1);
+        }
+
+        if(counter2 > arr.length/3){
+            list.add(voter2);
+        }
+
+        return list;
+    }
+
+}
