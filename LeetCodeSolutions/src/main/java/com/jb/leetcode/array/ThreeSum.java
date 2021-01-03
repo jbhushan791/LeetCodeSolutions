eba352328be04058ed064616d56b05c1b492cd5a
jbhushan791 <bhushan.jyoti791@gmail.com>
2021-01-03 02:25:06 -0500

    Three sum and two sum

---
+++ LeetCodeSolutions/src/main/java/com/jb/leetcode/array/ThreeSum.java
@@ -0,0 +1,52 @@
+package com.jb.leetcode.array;
+
+import java.util.*;
+
+/**
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ *
+ * Notice that the solution set must not contain duplicate triplets.
+ *
+ * @link https://leetcode.com/problems/3sum/
+ *
+ * @author jbhushan
+ */
+public class ThreeSum {
+
+    public static void main(String[] args){
+        int[] arr = {-1,0,1,2,-1,-4};
+
+        List<List<Integer>>  triplet = twoSum(arr, 0);
+
+        for(List<Integer> l : triplet){
+            System.out.println(l.get(0)+" "+l.get(1)+" "+l.get(2)+" ");
+        }
+    }
+
+    public static List<List<Integer>>  twoSum(int[] arr, int target) {
+        Arrays.sort(arr);
+        Set<List<Integer>> set = new HashSet<List<Integer>>();
+        for(int i=0; i<arr.length;i++) {
+            int l = i+1;
+            int r = arr.length - 1;
+            while (l < r) {
+                int sum = arr[i] + arr[r] + arr[l];
+                if (sum == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(arr[i]);
+                    list.add(arr[l]);
+                    list.add(arr[r]);
+                    set.add(list);
+                    r--;
+                } else if (sum < target) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
+}
---
+++ LeetCodeSolutions/src/main/java/com/jb/leetcode/array/TwoSum.java
@@ -0,0 +1,43 @@
+package com.jb.leetcode.array;
+
+/**
+ * Given an array of integers that is already sorted in ascending order, find two numbers
+ * such that they add up to a specific target number.
+ *
+ * The function twoSum should return indices of the two numbers such that
+ * they add up to the target, where index1 must be less than index2.
+ *
+ * @link https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+ *
+ * @author jbhushan
+ */
+public class TwoSum {
+
+    public static void main(String[] args){
+        int[] arr = {2,7,11,15};
+
+        int[] indices = twoSum(arr, 9);
+
+        for(int n : indices){
+            System.out.print(n+" ");
+        }
+    }
+
+    public static int[] twoSum(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length-1;
+        while(l<r){
+            int sum = arr[r]+arr[l];
+            if(sum == target){
+                break;
+            } else if (sum < target){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        int[] index = {l+1,r+1};
+        return index;
+    }
+
+}
