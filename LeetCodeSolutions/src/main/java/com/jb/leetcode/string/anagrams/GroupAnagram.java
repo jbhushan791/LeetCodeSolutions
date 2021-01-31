8a2267b68541bd486a20aed520f29bcffe316989
jbhushan791 <bhushan.jyoti791@gmail.com>
2021-01-30 19:16:19 -0500

    updated doc

--- LeetCodeSolutions/src/main/java/com/jb/leetcode/string/anagrams/GroupAnagram.java
+++ LeetCodeSolutions/src/main/java/com/jb/leetcode/string/anagrams/GroupAnagram.java
@@ -3,6 +3,14 @@ package com.jb.leetcode.string.anagrams;
 import java.util.*;
 
 /**
+ * Given an array of strings strs, group the anagrams together. You can return the answer
+ * in any order.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or
+ * phrase, typically using all the original letters exactly once.
+ *
+ * @Link https://leetcode.com/problems/group-anagrams/
+ *
  * @author jbhushan
  */
 public class GroupAnagram {
